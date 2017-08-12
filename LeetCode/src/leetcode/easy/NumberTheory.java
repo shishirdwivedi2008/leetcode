95cef387b1eb686b9d0643263a777dec5834c032
shishir <shishirdwivedi2008@gmail.com>
2017-08-12 11:57:55 +0530

    Signed-off-by: shishir <shishirdwivedi2008@gmail.com>

--- LeetCode/src/leetcode/easy/NumberTheory.java
+++ LeetCode/src/leetcode/easy/NumberTheory.java
@@ -2,24 +2,34 @@ package leetcode.easy;
 
 public class NumberTheory {
 
+	/**
+	 * lcm(x,y)=x*(y/gcd(x,y))
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public int findLCM(int x, int y) {
-		return (x * y) / findGCD(x, y);
+		//multiplicatgion of x and y can lead to large number which can overflow therfore first divide y and then multiply.
+		
+		return (x*y)/findGCD(x, y);
 	}
 
+	/**
+	 * gcd(x,0)=x
+	 * gcd(x,y)=gcd(y,y%x)
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public int findGCD(int x, int y) {
-		if(x==0||y==0)
-			return 0;
-		if (x == y)
+		if (y == 0)
 			return x;
-		if (x > y) {
-			findGCD(x - y, y);
-		}
-		return findGCD(x, y - x);
-		
+		else
+			return findGCD(y, x % y);
 	}
-	
+
 	public static void main(String[] args) {
-		NumberTheory theory=new NumberTheory();
-		System.out.println(theory.findLCM(15, 20));
+		NumberTheory theory = new NumberTheory();
+		System.out.println(theory.findLCM(78477, 15));
 	}
 }
